27cbf51db39fc823c96ff4f49445db54776aae53
almondshin <almondshin8@gmail.com>
2025-03-24 07:35:42 +0900

    Initial commit

--- src/main/java/org/example/adservice/domain/ad/AdRepository.java
+++ src/main/java/org/example/adservice/domain/ad/AdRepository.java
@@ -6,8 +6,6 @@ import org.example.common.domain.Repository;
 import java.util.List;
 
 public interface AdRepository extends Repository<Ad,AdId> {
-    Ad find(AdId id);
-
     boolean existsByName(String name);
 
     List<Ad> findAds();
